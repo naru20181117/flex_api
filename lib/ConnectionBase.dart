28b60bf78016ee6ee7bfef01031f4f8308a11cb7
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-04-19 05:35:31 +0900

    set: http and json decode method

---
+++ lib/ConnectionBase.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Class ConnectionBase {
+
+  Future<List> getHttpJSONData(String keyword) async {
+    String url = "https://pixabay.com/api/?key=21219754-2559eb5af350e03107360e696";
+    url += "&q=" + keyword;
+    url += "&image_type=photo";
+    var response = await http.get(Uri.encodeFull(url),
+    headers: {"Content-Type": "application/json"});
+    var jsonData = json.decode(response.body);
+
+    return jsonData["hits"];
+  }
+}
